c598ae427102b68e059813d2c153c5a37730d8fb
madchemi <seojinho996@gmail.com>
2025-05-20 02:39:17 +0900

    Updates

--- src/hooks/likes.js
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-export function useLike(){
-    let [like,setLike] = useState(0)
-    function addLike(){
-        setLike(a=>a+1)
-    }
-    return [like, addLike];
-}
